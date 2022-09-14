217361d1cf7ec6c7af4eb2385ca7b6f43b030a06
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-09-14 21:40:39 +0800

    so tired

---
+++ resources/views/components/filament-components/select-user-result.blade.php
@@ -0,0 +1,16 @@
+<div class="relative flex rounded-md">
+    <div class="flex">
+        <div class="px-2 py-3">
+            <div class="w-10 h-10">
+                <img  role="img" class="object-cover w-10 h-10 overflow-hidden rounded-full shadow" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />               
+            </div>
+        </div>
+ 
+        <div class="flex flex-col justify-center py-2 pl-3">
+            <p class="pb-1 text-sm font-bold">{{ $name }}</p>
+            <div class="flex flex-col items-start">
+                <p class="text-xs leading-5">{{ $email }}</p>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/cash-advances/activity.blade.php
+++ resources/views/livewire/cash-advances/activity.blade.php
@@ -2,8 +2,5 @@
     <form wire:submit.prevent="submit">
         {{ $this->form }}
      
-        <button type="submit">
-            Submit
-        </button>
     </form>
 </div>
